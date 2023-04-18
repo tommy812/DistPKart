a4e61d8e98d1d8d820816c2b2efe3a581da7571a
tommy812 <39319157+tommy812@users.noreply.github.com>
2023-04-18 21:37:15 +0100

    Final Submition

--- src/AnimationPanel.java
+++ src/AnimationPanel.java
@@ -1,3 +1,9 @@
+///////////////////////////////////////////
+//
+//  Sid: 1955004
+//
+///////////////////////////////////////////
+
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,33 +15,31 @@ import javax.swing.*;
 public class AnimationPanel extends JPanel{
     private final static String RED_KART = "KartRed"; //standard name of the images Red kart
     private final static String BLUE_KART = "KartBlue"; //standard name of the images Blue kart
-    protected ImageIcon imagesR[];// array containing the images of Red Karts
-    protected ImageIcon imagesB[];// array containing the images of Blue Karts
+    protected ImageIcon images[];// array containing the images of Red Karts
     private final int TOT_IMAGES = 16;
     private int currentImg = 0;//img index
     private final int ANIMATION_DELAY = 100; // ms delay
     private Timer animationTimer;//Swing timer
 
     public AnimationPanel(){
-        imagesR = new ImageIcon[TOT_IMAGES];
-        //imagesB = new ImageIcon[TOT_IMAGES];
+        images = new ImageIcon[TOT_IMAGES];
 
-        //loading all 32 Images
-        // In two different arrays, one for each Kart colour
-        for(int i=0;i<imagesR.length;i++){
-            imagesR[i] = new ImageIcon(getClass().getResource("Karts/KartRed/"+RED_KART+i+".png"));
-            //imagesB[i] = new ImageIcon(getClass().getResource("Karts/"+BLUE_KART +i+".png"));
+        //loading all 16 Images in an array
+        for(int i=0;i<images.length;i++){
+            images[i] = new ImageIcon(getClass().getResource("Karts/KartRed/"+RED_KART+i+".png"));
         }//For loop
     }//AnimationPanel
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        imagesR[currentImg].paintIcon(this,g,0,0);
+        images[currentImg].paintIcon(this,g,0,0);
 
         if(animationTimer.isRunning()){
             currentImg = (currentImg+1) % TOT_IMAGES;
         }//if statement
     }//PaintComponent()
+
+
     public void startAnimation(){
         if (animationTimer == null){
             currentImg =0;//display image0
